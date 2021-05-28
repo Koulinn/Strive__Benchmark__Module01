2325f1889d2a0f7e5f2f244b2d1f17c748c5a00d
Koulinn <drdverzola@gmail.com>
2021-05-28 12:25:25 +0100

    Dinnamically swap content from show-result div

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const questionsBank = {"response_code":0,"results":[{"category":"Science: Computers","type":"multiple","difficulty":"easy","question":"When Gmail first launched, how much storage did it provide for your email?","correct_answer":"1GB","incorrect_answers":["512MB","5GB","Unlimited"]},{"category":"Science: Computers","type":"multiple","difficulty":"easy","question":"What does the Prt Sc button do?","correct_answer":"Captures what&#039;s on the screen and copies it to your clipboard","incorrect_answers":["Nothing","Saves a .png file of what&#039;s on the screen in your screenshots folder in photos","Closes all windows"]},{"category":"Science: Computers","type":"boolean","difficulty":"easy","question":"Pointers were not used in the original C programming language; they were added later on in C++.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Science: Computers","type":"boolean","difficulty":"easy","question":"Ada Lovelace is often considered the first computer programmer.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Science: Computers","type":"boolean","difficulty":"easy","question":"In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Science: Computers","type":"boolean","difficulty":"easy","question":"The Windows ME operating system was released in the year 2000.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Science: Computers","type":"multiple","difficulty":"easy","question":"The numbering system with a radix of 16 is more commonly referred to as ","correct_answer":"Hexidecimal","incorrect_answers":["Binary","Duodecimal","Octal"]},{"category":"Science: Computers","type":"multiple","difficulty":"easy","question":"The C programming language was created by this American computer scientist. ","correct_answer":"Dennis Ritchie","incorrect_answers":["Tim Berners Lee","al-Khw\u0101rizm\u012b","Willis Ware"]},{"category":"Science: Computers","type":"multiple","difficulty":"easy","question":"How long is an IPv6 address?","correct_answer":"128 bits","incorrect_answers":["32 bits","64 bits","128 bytes"]},{"category":"Science: Computers","type":"multiple","difficulty":"easy","question":"What does the computer software acronym JVM stand for?","correct_answer":"Java Virtual Machine","incorrect_answers":["Java Vendor Machine","Java Visual Machine","Just Virtual Machine"]}]}
+const questionsBank = { "response_code": 0, "results": [{ "category": "Science: Computers", "type": "multiple", "difficulty": "easy", "question": "When Gmail first launched, how much storage did it provide for your email?", "correct_answer": "1GB", "incorrect_answers": ["512MB", "5GB", "Unlimited"] }, { "category": "Science: Computers", "type": "multiple", "difficulty": "easy", "question": "What does the Prt Sc button do?", "correct_answer": "Captures what&#039;s on the screen and copies it to your clipboard", "incorrect_answers": ["Nothing", "Saves a .png file of what&#039;s on the screen in your screenshots folder in photos", "Closes all windows"] }, { "category": "Science: Computers", "type": "boolean", "difficulty": "easy", "question": "Pointers were not used in the original C programming language; they were added later on in C++.", "correct_answer": "False", "incorrect_answers": ["True"] }, { "category": "Science: Computers", "type": "boolean", "difficulty": "easy", "question": "Ada Lovelace is often considered the first computer programmer.", "correct_answer": "True", "incorrect_answers": ["False"] }, { "category": "Science: Computers", "type": "boolean", "difficulty": "easy", "question": "In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.", "correct_answer": "True", "incorrect_answers": ["False"] }, { "category": "Science: Computers", "type": "boolean", "difficulty": "easy", "question": "The Windows ME operating system was released in the year 2000.", "correct_answer": "True", "incorrect_answers": ["False"] }, { "category": "Science: Computers", "type": "multiple", "difficulty": "easy", "question": "The numbering system with a radix of 16 is more commonly referred to as ", "correct_answer": "Hexidecimal", "incorrect_answers": ["Binary", "Duodecimal", "Octal"] }, { "category": "Science: Computers", "type": "multiple", "difficulty": "easy", "question": "The C programming language was created by this American computer scientist. ", "correct_answer": "Dennis Ritchie", "incorrect_answers": ["Tim Berners Lee", "al-Khw\u0101rizm\u012b", "Willis Ware"] }, { "category": "Science: Computers", "type": "multiple", "difficulty": "easy", "question": "How long is an IPv6 address?", "correct_answer": "128 bits", "incorrect_answers": ["32 bits", "64 bits", "128 bytes"] }, { "category": "Science: Computers", "type": "multiple", "difficulty": "easy", "question": "What does the computer software acronym JVM stand for?", "correct_answer": "Java Virtual Machine", "incorrect_answers": ["Java Vendor Machine", "Java Visual Machine", "Just Virtual Machine"] }] }
 const Allquestions = questionsBank.results
 const questionContainer = document.getElementById('questions-container')
 
 window.onload = () => {
-    
+
     createHTMLForEachQuestion(Allquestions)
 
 }
@@ -20,19 +20,14 @@ window.onload = () => {
 
 
 
-// function selectEachDivWithClassQuestion(){
-//     let getQuestionsContainers = document.getElementsByClassName('question')
-//     return getQuestionsContainers
-// }
 
-// let listOfDivSpacesForEachQuestion = selectEachDivWithClassQuestion()
 
 function createHTMLForEachQuestion(questions) {
-    
-    for(question of questions){
-        let indexOfEachQuestion = questions.indexOf(question)     
-        
-        if(question.type === 'boolean'){
+
+    for (question of questions) {
+        let indexOfEachQuestion = questions.indexOf(question)
+
+        if (question.type === 'boolean') {
             questionContainer.insertAdjacentHTML('beforeend', `
             <div class='question boolean'>
                 <h3>Question -> ${question.question}</h3>
@@ -47,10 +42,10 @@ function createHTMLForEachQuestion(questions) {
                 </div>    
             </div>`)
 
-           
+
         }
-        
-        if(question.type === 'multiple') {
+
+        if (question.type === 'multiple') {
             questionContainer.insertAdjacentHTML('beforeend', `
             <div class='question multiple'>
             <h3>Question -> ${question.question}</h3>
@@ -70,21 +65,39 @@ function createHTMLForEachQuestion(questions) {
                     
                 </div>
             
-            </div>`)
-           
+            </div>
+            `)
+
         }
     }
 
 }
 
-
-function checkPoints(){
+function checkPoints() {
     let allInputs = document.getElementsByTagName('input')
     let points = 0
-    for(let input of allInputs){
-        if(input.checked  && (input.value === '1')){
+    for (let input of allInputs) {
+        if (input.checked && (input.value === '1')) {
             points++
         }
     }
     return points
 }
+
+function showTotalPoints() {
+    let totalPoints = checkPoints()
+    let mainHTML = document.getElementsByTagName('main')[0]
+
+    if (document.getElementById('show-result') === null) {
+        mainHTML.insertAdjacentHTML('afterbegin', `
+        <div id="show-result"><h2> You got ${totalPoints} from 10</h2></div>
+        `)
+    } else {
+        let showResultDiv = document.getElementById('show-result')
+        console.log()
+        showResultDiv.innerHTML = `<h2> You got ${totalPoints} from 10</h2>`
+    }
+}
+
+
+
